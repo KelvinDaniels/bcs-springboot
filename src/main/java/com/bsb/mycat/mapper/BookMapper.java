61c0e15c7896409378fdb956d71ff62098da2263
wangyu <wang.wy@bitsun-inc.com>
2020-11-05 13:51:36 +0800

    初始化

---
+++ src/main/java/com/bsb/mycat/mapper/BookMapper.java
@@ -0,0 +1,22 @@
+package com.bsb.mycat.mapper;
+
+
+import com.bsb.mycat.model.Book;
+
+import java.util.List;
+
+public interface BookMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Book record);
+
+    int insertSelective(Book record);
+
+    Book selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Book record);
+
+    int updateByPrimaryKey(Book record);
+
+    List<Book> selectAllBook();
+ }
